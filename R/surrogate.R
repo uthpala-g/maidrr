c65c585aff08b5c909e880714034415ae3af06d3
uthpala-g <uthpala-gunathilake@uiowa.edu>
2025-11-04 13:16:30 -0600

    Update surrogate.R

--- R/surrogate.R
+++ R/surrogate.R
@@ -41,7 +41,7 @@ surrogate <- function(data, par_list) {
 
     # UG Edits
     cat_vars <- sapply(data[vars_form], is.factor)
-    for (var in vars_form[cat_vars]) {
+    for (var in vars_form[cat_vars]) {
       levels <- levels(data[[var]])
       if (length(levels) > 1) { # Only relevel if there are multiple levels
         # Sort the levels alphabetically
